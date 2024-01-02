2d5c4a2610676b90477f1214cd6c2e4bd2d0c2b9
AJ Sorbello <ajsorbello@gmail.com>
2024-01-02 12:47:46 -0800

    jwtstrategy updated

--- passport.js
+++ passport.js
@@ -45,16 +45,15 @@ passport.use(
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
+  secretOrKey: process.env.JWT_SECRET // Use environment variable
 }, async (jwtPayload, callback) => {
   return await Users.findById(jwtPayload._id)
     .then((user) => {
+      console.log(`User with ID ${user._id} authenticated`); // Log user ID instead of entire user object
       return callback(null, user);
     })
     .catch((error) => {
-       if (error) {
-          console.log(error);
+      console.log(error);
       return callback(error);
-       }
     });
 }));
