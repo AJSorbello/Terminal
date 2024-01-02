29812ee232a9c015f1e80368410e686f7c7c6ba5
AJ Sorbello <ajsorbello@gmail.com>
2024-01-02 12:33:06 -0800

    updated passport auth in index

--- index.js
+++ index.js
@@ -235,10 +235,9 @@ app.get("/movies", passport.authenticate("jwt", { session: false }),
 });
 // POST: Add a movie to a user's list of favorites
 app.post(
-  "/users/:Username/movies/:MovieID",
+  "/users/:Username/movies/:MovieID", passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     try {
-      passport.authenticate("jwt", { session: false });
       const updatedUser = await Users.findOneAndUpdate(
         { Username: req.params.Username },
         { $push: { FavoriteMovies: req.params.MovieID } },
