dd4c9163f2e87dbce7f53dafce602bdc8179bff0
AJ Sorbello <ajsorbello@gmail.com>
2024-02-15 17:10:11 -0800

    added form url

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 app.use(morgan("common"));
 
 
-let allowedOrigins = ['http://localhost:8080', 'https://bellomovies.netlify.app', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'https://bellomovies.netlify.app', 'http://localhost:1234', 'https://bellomoviesfork.netlify.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
